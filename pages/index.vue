f57c1ac21f3ccdbd02a4eb9e098e7e4392651fb2
unknown <bensonagala@live.com>
2023-08-04 12:52:03 +0300

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Navbar/>
+        <Hero/>
+
+        <Modal/>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
