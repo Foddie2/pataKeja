de86dd4eb7b74fc71085580f5113b724eafb77c1
unknown <bensonagala@live.com>
2023-08-05 12:47:55 +0300

    Home page setup

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,13 @@
 </template>
 
 <script>
-export default {};
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+}); // export default {};
 </script>
 
 <style lang="scss" scoped></style>
