c9d3c30244b220a9f7dd95b721fdc3ad71d4c956
Foddie2 <Bensonagala@live.com>
2023-08-18 22:08:30 +0300

    project setup

--- components/Hero.vue
+++ components/Hero.vue
@@ -164,7 +164,13 @@
 </template>
 
 <script>
-export default {};
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <style lang="scss" scoped></style>
